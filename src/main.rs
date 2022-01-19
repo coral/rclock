530f305e33f29524c62b585417f569784fcfa8f7
coral <jonas@jonasbengtson.se>
2022-01-18 21:17:16 -0800

    fml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,102 @@
-fn main() {
+use anyhow::bail;
+use anyhow::Result;
+use log::*;
+use smol;
+use std::sync::Arc;
+use url;
+
+use esp_idf_svc::netif::*;
+use esp_idf_svc::nvs::*;
+use esp_idf_svc::sysloop::*;
+use esp_idf_sys;
+use esp_idf_sys::esp;
+
+use embedded_svc::utils::anyerror::*;
+use embedded_svc::wifi::*;
+use esp_idf_svc::wifi::*;
+
+const SSID: &str = "Coral";
+const PASS: &str = "hamtgurka";
+
+fn main() -> Result<()> {
     esp_idf_sys::link_patches();
+
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    #[allow(unused)]
+    let netif_stack = Arc::new(EspNetifStack::new()?);
+    #[allow(unused)]
+    let sys_loop_stack = Arc::new(EspSysLoopStack::new()?);
+    #[allow(unused)]
+    let default_nvs = Arc::new(EspDefaultNvs::new()?);
+
+    #[allow(unused_mut)]
+    let mut wifi = wifi(
+        netif_stack.clone(),
+        sys_loop_stack.clone(),
+        default_nvs.clone(),
+    )?;
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn wifi(
+    netif_stack: Arc<EspNetifStack>,
+    sys_loop_stack: Arc<EspSysLoopStack>,
+    default_nvs: Arc<EspDefaultNvs>,
+) -> Result<Box<EspWifi>> {
+    let mut wifi = Box::new(EspWifi::new(netif_stack, sys_loop_stack, default_nvs)?);
+
+    info!("Wifi created, about to scan");
+
+    let ap_infos = wifi.scan()?;
+
+    let ours = ap_infos.into_iter().find(|a| a.ssid == SSID);
+
+    let channel = if let Some(ours) = ours {
+        info!(
+            "Found configured access point {} on channel {}",
+            SSID, ours.channel
+        );
+        Some(ours.channel)
+    } else {
+        info!(
+            "Configured access point {} not found during scanning, will go with unknown channel",
+            SSID
+        );
+        None
+    };
+
+    wifi.set_configuration(&Configuration::Mixed(
+        ClientConfiguration {
+            ssid: SSID.into(),
+            password: PASS.into(),
+            channel,
+            ..Default::default()
+        },
+        AccessPointConfiguration {
+            ssid: "aptest".into(),
+            channel: channel.unwrap_or(1),
+            ..Default::default()
+        },
+    ))?;
+
+    info!("Wifi configuration set, about to get status");
+
+    let status = wifi.get_status();
+
+    if let Status(
+        ClientStatus::Started(ClientConnectionStatus::Connected(ClientIpStatus::Done(ip_settings))),
+        ApStatus::Started(ApIpStatus::Done),
+    ) = status
+    {
+        info!("Wifi connected");
+
+        //ping(&ip_settings)?;
+    } else {
+        bail!("Unexpected Wifi status: {:?}", status);
+    }
+
+    Ok(wifi)
 }
